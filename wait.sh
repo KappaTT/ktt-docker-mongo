9db32e597662bc679ce248c1d842fc1d1a6b7540
Jeff Taylor-Chang <jeff@jefftc.com>
2020-04-24 16:33:25 -0400

    Separated starting and waiting scripts

---
+++ wait.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd "$(dirname "$0")"
+
+mongo_docker_ready() {
+    echo 'db.runCommand("ping").ok' | mongo localhost:27017/test --quiet > /dev/null
+}
+
+while !(mongo_docker_ready); do
+    sleep 1
+done
+
+echo "Mongo docker is ready"
